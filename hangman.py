c02b80ed132ed3c8260a6e55290c6bc5f6026501
Shruti Rao <shrutirao94@gmail.com>
2020-04-15 22:32:06 +0200

    Hangman Algorithm: User Input
    User Input: Word chosen by algorithm is converted to a list.
    User Input has to be verified to ensure that it is a single, valid,
    English letter. User is notified otherwise.

--- hangman.py
+++ hangman.py
@@ -18,7 +18,21 @@ def hangman_game_display(word):
     print('Hint: It\'s a city!') 
     return no_of_dashes
 
+def hangman_algorithm(word, user_guess):
+    """
+    Some text here
+    """
+    user_guess_list = []
+    word_list = list(word)
+    print(word_list)
+    if (len(user_guess) == 1 and user_guess.isalpha()):
+        print("Valid Input")
+    else:
+        print("Please enter a valid input. Only a single English letter is allowed.")
+    return user_guess
+
 
 if __name__ == '__main__':
     # print("RANDOM WORD:", pick_word())
-    hangman_game_display(pick_word())
+    # hangman_game_display(pick_word())
+    hangman_algorithm(pick_word(), "A")
--- tests.py
+++ tests.py
@@ -12,5 +12,9 @@ class TestStringMethods(unittest.TestCase):
         dash_length = hangman.hangman_game_display(word)
         self.assertTrue(dash_length == len(word))
 
+    def test_valid_user_guess(self):
+        user_guess = hangman.hangman_algorithm(self, self)
+        self.assertTrue(user_guess.isalpha()) 
+
 if __name__ == '__main__':
     unittest.main()
