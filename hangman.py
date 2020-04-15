779b0638a83ecdcb182b963f46937e4885732ec2
Shruti Rao <shrutirao94@gmail.com>
2020-04-15 21:56:58 +0200

    Hangman Game Display: Dashes and Hint
    Method displays the dashes for the user based on the randomly selected
    word. It also prints a standard hint for the user.
    
    Note: At this time, all dashes are left empty for the user to guess. An
    extension would be to randomly populate a dash to reveal a letter.

--- hangman.py
+++ hangman.py
@@ -5,10 +5,20 @@ def pick_word():
     """
     Randomly picks a word from the words.py file and converts to upper case.
     """
-
     word = random.choice(cities).upper()
     return word
 
+def hangman_game_display(word):
+    """
+    Generates a standard hint and the number of dashes based on the word
+    previously selected 
+    """
+    no_of_dashes = len(word)
+    print('_ ' * no_of_dashes, '\n')
+    print('Hint: It\'s a city!') 
+    return no_of_dashes
+
 
 if __name__ == '__main__':
-    print("RANDOM WORD:", pick_word())
+    # print("RANDOM WORD:", pick_word())
+    hangman_game_display(pick_word())
--- tests.py
+++ tests.py
@@ -1,11 +1,16 @@
 import unittest
-from hangman import pick_word
+import hangman 
 
 class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
-        word = pick_word()
+        word = hangman.pick_word()
         self.assertTrue(word.upper())
+
+    def test_no_of_dashes(self):
+        word = hangman.pick_word()
+        dash_length = hangman.hangman_game_display(word)
+        self.assertTrue(dash_length == len(word))
 
 if __name__ == '__main__':
     unittest.main()
