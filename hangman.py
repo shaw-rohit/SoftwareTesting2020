b06e846836167ab5d28589bd2c83862bb9bf7c0b
Shruti Rao <shrutirao94@gmail.com>
2020-04-16 15:37:50 +0200

    User Display
    Corrected and added user information:
     1. Number of attempts remaning
     2. Letters already used up
     3. Re-printing dashes
     4. Initial game state

--- hangman.py
+++ hangman.py
@@ -1,14 +1,15 @@
 import random
 from words import cities
 
-def pick_word():
+def hangman_pick_word():
     """
     Randomly picks a word from the words.py file and converts to upper case.
     """
     word = random.choice(cities).upper()
     return word
 
-def hangman_game_display(word):
+
+def hangman_print_dashes(word):
     """
     Generates a standard hint and the number of dashes based on the word
     previously selected 
@@ -16,12 +17,13 @@ def hangman_game_display(word):
     no_of_dashes = len(word)
     print_dashes = '_' * no_of_dashes 
     print(print_dashes, '\n')
-    print('Hint: It\'s a city!') 
     return print_dashes
 
-def play_hangman():
+
+def hangman_get_letter():
     user_guess = input("Please guess a letter! ").upper()
     return user_guess
+
 
 def hangman_algorithm(word):
     """
@@ -30,25 +32,31 @@ def hangman_algorithm(word):
     user_guess_list = []
     attempts = 9
     guessed = False
-    print_dashes = hangman_game_display(word)
+
+    print("Starting game.....")
+    print_dashes = hangman_print_dashes(word)
+    print("Hint: It\'s a city!")
+
     print_dashes_list = list(print_dashes)
-
-
     word_list = list(word)
-    print(word_list)
 
     while not guessed and attempts > 0:
-        user_guess = play_hangman()
+        print("Remaining Lives: {0} ".format(attempts))
+        print("Letters Guessed: {0} ".format(user_guess_list))
+        user_guess = hangman_get_letter()
+
         if (len(user_guess) == 1 and user_guess.isalpha()):
-            print("User Guessed: ", user_guess)
             if user_guess in user_guess_list:
-                print(" You've already guessed this word! Try another valid letter ")
+                print("You've already guessed this letter! Try again!")
+                print(print_dashes)
             elif user_guess not in word:
-                print(" Sorry! ", user_guess, " is not in the word. Try again!")
+                print("Sorry! ", user_guess, " is not in the word. Try again!")
                 user_guess_list.append(user_guess)
                 attempts -= 1
+                print(print_dashes)
             else:
-                print("User entered a correct guess!")
+                # Correct Guess
+                print("This letter is in the word!")
                 user_guess_list.append(user_guess)
 
                 valid_indices = []
@@ -56,13 +64,11 @@ def hangman_algorithm(word):
                 for index, letter in enumerate(word):
                     if letter == user_guess:
                         valid_indices += [index]
-                print(valid_indices)
-
 
                 for index in valid_indices:
                     print_dashes_list[index] = user_guess
 
-                print_dashes = " ".join(print_dashes_list)
+                print_dashes = "".join(print_dashes_list)
                 print(print_dashes)
                 
                 if '_' not in print_dashes:
@@ -76,5 +82,6 @@ def hangman_algorithm(word):
     else:
         print("Sorry! You ran out of lives!")
 
+
 if __name__ == '__main__':
-    hangman_algorithm(pick_word())
+    hangman_algorithm(hangman_pick_word())
