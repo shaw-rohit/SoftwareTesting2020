4d3031386d2a225b91707ffe3aa4db1a6cb6af77
Shruti Rao <shrutirao94@gmail.com>
2020-04-16 14:06:47 +0200

    Hangman Algorithm
    The core game logic whereby user gets 9 attempts.
    As long as the word is not guessed, each guess is checked for validity.
    If the guess is valid, the occurances of the guessed letter and their
    corresponding indices are obtained. The dashes are replaced with the
    correctly guessed letters and the game continues.
    
    Once all dashes used up and the user still has lives left, the game is
    considered won.
    
    If user finishes up all attemps with blanks left, the user loses the
    game.

--- hangman.py
+++ hangman.py
@@ -14,9 +14,10 @@ def hangman_game_display(word):
     previously selected 
     """
     no_of_dashes = len(word)
-    print('_ ' * no_of_dashes, '\n')
+    print_dashes = '_' * no_of_dashes 
+    print(print_dashes, '\n')
     print('Hint: It\'s a city!') 
-    return no_of_dashes
+    return print_dashes
 
 def play_hangman():
     user_guess = input("Please guess a letter! ").upper()
@@ -28,17 +29,52 @@ def hangman_algorithm(word):
     """
     user_guess_list = []
     attempts = 9
+    guessed = False
+    print_dashes = hangman_game_display(word)
+    print_dashes_list = list(print_dashes)
+
 
     word_list = list(word)
     print(word_list)
 
-    while not user_guess_list and attempts > 0:
+    while not guessed and attempts > 0:
         user_guess = play_hangman()
         if (len(user_guess) == 1 and user_guess.isalpha()):
             print("User Guessed: ", user_guess)
+            if user_guess in user_guess_list:
+                print(" You've already guessed this word! Try another valid letter ")
+            elif user_guess not in word:
+                print(" Sorry! ", user_guess, " is not in the word. Try again!")
+                user_guess_list.append(user_guess)
+                attempts -= 1
+            else:
+                print("User entered a correct guess!")
+                user_guess_list.append(user_guess)
+
+                valid_indices = []
+
+                for index, letter in enumerate(word):
+                    if letter == user_guess:
+                        valid_indices += [index]
+                print(valid_indices)
+
+
+                for index in valid_indices:
+                    print_dashes_list[index] = user_guess
+
+                print_dashes = " ".join(print_dashes_list)
+                print(print_dashes)
+                
+                if '_' not in print_dashes:
+                    guessed = True
+
         else:
             print("Please enter a valid input. Only a single English letter is allowed.")
 
+    if guessed:
+        print("Yay! You won the game!")
+    else:
+        print("Sorry! You ran out of lives!")
 
 if __name__ == '__main__':
     hangman_algorithm(pick_word())
