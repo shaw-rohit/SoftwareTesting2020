2049d7a1aebd7a934d6d3fe1729831c879db6f34
Shruti Rao <shrutirao94@gmail.com>
2020-04-28 17:49:24 +0200

    Changes to Code Structure for Creating Executable
    `hangman.py` was restructured to have a `__main__.py` file that now has
    a `main()` method. Outside of the `hangman/`dir, a `cli.py` was created
    that allows for `hangman/__main__.py` to be an entrypoint for the app.
    
    An executable was created based on this new struture using PyInstaller.
    
    To now run the code from the shell please call cli.py

---
+++ hangman/__main__.py
@@ -0,0 +1,39 @@
+import time
+from hangman.game import Game
+from hangman.interface import Interface
+from hangman.player import Player
+
+def main():
+    interface = Interface()
+
+    replay = "Y"
+
+    menu_input_flag = 0
+
+    while menu_input_flag == 0:
+        interface.clear_screen()
+        interface.write_title()
+        interface.write_options()
+        menu_choice = interface.menu_choice()
+
+        if menu_choice == "1":
+            menu_input_flag = 1
+            interface.clear_screen()
+            while replay[0].upper() == "Y":
+                interface.clear_screen()
+                player = Player()
+                game = Game(player, interface)
+                game.start()
+                replay = interface.read_replay()
+
+        elif menu_choice == "2":
+            interface.clear_screen()
+            interface.write_title()
+            interface.write_instructions()
+
+        elif menu_choice == "3":
+            exit()
+
+        else:
+            print("Invalid input. Please try again.")
+            time.sleep(0.5)
