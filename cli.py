2b30175b6989a87dacee8c286a434bf1c55b7509
Mr. Robot <shaw.rohit@gmail.com>
2020-04-30 17:17:35 +0200

    Exception KeyboardInterrupt and EOFError suppressed

--- cli.py
+++ cli.py
@@ -1,4 +1,12 @@
 from hangman.__main__ import main
+import sys
 
-if __name__ == '__main__':
-    main()
+try:
+    if __name__ == '__main__':
+        main()
+
+except KeyboardInterrupt:
+    sys.exit(0)
+
+except EOFError:
+    sys.exit(0)
