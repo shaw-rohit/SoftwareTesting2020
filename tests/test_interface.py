8ba5926feae5e52c237b83748cfb8470253dd390
Shruti Rao <shrutirao94@gmail.com>
2020-05-04 00:35:38 +0200

    Tests for `read_palyer_guess`
    Tested above method under `test_read_player_guess` to see if all letters
    were correctly converted to upper case and if all other inputs were not
    converted.

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -6,10 +6,35 @@ from hangman.interface import Interface
 
 def test_read_player_guess(monkeypatch):
     """
-    Data Value Test
+    Data integrity test to check for upper case conversion of input
     """
     with monkeypatch.context() as m:
         m.setattr('builtins.input', lambda prompt="": "M")
         interface = Interface()
         assert interface.read_player_guess().isupper()
+
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda prompt="": "m")
+        interface = Interface()
+        assert interface.read_player_guess().isupper()
+
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda prompt="": "mm")
+        interface = Interface()
+        assert interface.read_player_guess().isupper()
+
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda prompt="": "1")
+        interface = Interface()
+        assert not interface.read_player_guess().isupper()
+
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda prompt="": "@")
+        interface = Interface()
+        assert not interface.read_player_guess().isupper()
+    
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda prompt="": " ")
+        interface = Interface()
+        assert not interface.read_player_guess().isupper()
     
