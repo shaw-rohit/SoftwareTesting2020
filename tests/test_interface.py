3848c7c6f4415e89e477a4f0610adc053766f315
Shruti Rao <shrutirao94@gmail.com>
2020-05-04 16:23:48 +0200

    `test_write_instructions`
    This method had a control flow logic and initial values check. It also
    includes veryfying user input to ensure it is always upper case.

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -66,12 +66,22 @@ def test_read_player_guess(monkeypatch):
 
     def test_write_title():
         """
+        Output Correctness test to check if hangman banner is correct
         """
-        pass
-        # HANGMAN_TITLE = 
-        # interface = Interface()
-        # title = capsys.readouterr()
-        # assert title.out == HANGMAN_TITLE
+        title_line1 = " _                                           "
+        title_line2 = "| |                                            "
+        title_line3 = "| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  "
+        title_line4 = "| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ "
+        title_line5 = "| | | | (_| | | | | (_| | | | | | | (_| | | | |"
+        title_line6 = "|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|"
+        title_line7 = "                    __/ |                      "
+        title_line8 = "                   |___/                       "
+        HANGMAN_TITLE = title_line1 + title_line2 + title_line3 + \
+                title_line4 + title_line5 + title_line6 + \
+                title_line7 + title_line8
+        interface = Interface()
+        title = capsys.readouterr()
+        assert title.out == HANGMAN_TITLE
 
     def test_write_options(capsys):
         """
@@ -87,7 +97,52 @@ def test_read_player_guess(monkeypatch):
         options = capsys.readouterr()
         assert options.out == PRINT_INSTRUCTIONS
 
+    def test_menu_choice(monkeypatch):
+        """
+        Data Validity Test for menu choice ensures that user does provide
+        input
+        """
+        with monkeypatch.context() as m:
+            m.setattr('builtins.input', lambda prompt="":"1")
+            interface = Interface()
+            response = interface.menu_choice()
+            assert len(response) > 0
 
+
+    def test_write_instructions(capsys, monkeypatch):
+        """
+        Ouptput correctness test to ensure that instructions are printed on
+        screen
+        Control Flow Check to ensure that that the correct conditions are
+        executed based on choice
+        Initial Values test checks validity of input_flag's initialized value
+        and updated value
+        """
+
+        interface = Interface()
+        INPUT_FLAG_ZERO = 0
+        INPUT_FLAG_ONE = 1
+
+        with monkeypatch.context() as m:
+            m.setattr('builtins.input', lambda prompt="":"x")
+            interface = Interface()
+            response = interface.write_instructions().input_flag
+            assert response == INPUT_FLAG_ONE
+
+        with monkeypatch.context() as m:
+            m.setattr('builtins.input', lambda prompt="":"w")
+            interface = Interface()
+            response = interface.write_instructions().input_flag
+            assert response != INPUT_FLAG_ONE
+
+        with monkeypatch.context() as m:
+            m.setattr('builtins.input', lambda prompt="":"x")
+            interface = Interface()
+            response = interface.write_instructions().choice
+            assert choice.isupper()
+        
+        initial_input_flag = interface.write_instructions.input_flag
+        assert initial_input_flag == INPUT_FLAG_ZERO
 
     def test_write_instructions():
         """
